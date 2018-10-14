43c38ac00fcff9b3a2a804aaaf484a279f9b926e
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-10-14 16:09:13 -0700

    - add swipe x and swipe y

--- micky-mouse-gesture/ViewController.swift
+++ micky-mouse-gesture/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var gestureStartPoint: CGPoint!;
+    
+    private let minimumGestureLength = Float(25);
+    private let maximumVarience = Float(5);
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touches: NSSet! = event?.allTouches as! NSSet;
+        let touch = touches.anyObject() as! UITouch;
+        gestureStartPoint = touch.location(in: self.view);
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touches: NSSet! = event?.allTouches as! NSSet;
+        let touch = touches.anyObject() as! UITouch;
+        let currentPosition = touch.location(in: self.view);
+        
+        
+        let deltaX = fabsf(Float(gestureStartPoint.x - currentPosition.x));
+        let deltaY = fabsf(Float(gestureStartPoint.y - currentPosition.y));
+        
+        if(deltaX >= minimumGestureLength && deltaY <= maximumVarience) {
+            print("swipe X")
+        }
+        
+        if(deltaY >= minimumGestureLength && deltaX <= maximumVarience) {
+            print("swipe Y")
+        }
+    }
+    
+    
 
 }
 
