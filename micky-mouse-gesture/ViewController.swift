595d642e95c62a3fb54ee38178ae1230c857d7e1
Pattarasai Markpeng <pattarasaimarkpeng@Pattarasais-MacBook-Pro.local>
2018-10-14 16:30:12 -0700

    - finish time gesture

--- micky-mouse-gesture/ViewController.swift
+++ micky-mouse-gesture/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     private var gestureStartPoint: CGPoint!;
-    
-    private let minimumGestureLength = Float(25);
+    private let minimumGestureLength = Float(40);
     private let maximumVarience = Float(5);
+    
+    @IBOutlet weak var timeCounter: UILabel!;
+    var timeMinCount = 1;
+    var timeSecCount = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,11 +38,24 @@ class ViewController: UIViewController {
         let deltaY = fabsf(Float(gestureStartPoint.y - currentPosition.y));
         
         if(deltaX >= minimumGestureLength && deltaY <= maximumVarience) {
-            print("swipe X")
-        }
-        
-        if(deltaY >= minimumGestureLength && deltaX <= maximumVarience) {
-            print("swipe Y")
+            if(Float(gestureStartPoint.x - currentPosition.x) <= 0.0) {
+                timeSecCount = timeSecCount + 10;
+            } else {
+                timeSecCount = timeSecCount - 10;
+            }
+            
+            if(timeSecCount >= 60) {
+                timeMinCount = timeMinCount + 1;
+                timeSecCount = 0;
+            }
+            
+            if(timeSecCount < 0) {
+                timeMinCount = timeMinCount-1 <= 0 ? 0 : timeMinCount-1;
+                timeSecCount = 0;
+            }
+            
+            
+            timeCounter.text = "\(String(format: "%02d", timeMinCount)):\(String(format: "%02d", timeSecCount))"
         }
     }
     
